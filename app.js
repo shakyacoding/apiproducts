467f39ebda132f2594b1ead85de5d81486352931
shakyacoding <100017645+shakyacoding@users.noreply.github.com>
2024-09-08 12:17:35 +0530

    final

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 // Enable CORS for all routes
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://localhost:5173',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   headers: ['Content-Type', 'Authorization'],
